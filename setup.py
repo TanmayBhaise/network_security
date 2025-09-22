93fe67c13bbce05c7ea47e6e90383919db445db2
TanmayBhaise <tanmaybhaise143@gmail.com>
2025-09-22 09:14:20 +0530

    Exception and logging

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 from setuptools import find_packages, setup
 from typing import List
 
+
 def get_reuirements() -> List:
     requirement_list: List[str] = []
     try:
-        with open('requirments.txt', 'r') as file:
+        with open("requirments.txt", "r") as file:
             lines = file.readline()
             for line in lines:
                 requirement = line.strip()
-                if requirement and requirement!='-e.':
+                if requirement and requirement != "-e.":
                     requirement_list.append(requirement)
 
-        
     except FileNotFoundError:
         print("requirement.txt file not found")
-    
+
     return requirement_list
+
 
 setup(
     name="network_security",
@@ -23,5 +24,5 @@ setup(
     author="Tanmay Bhaise",
     author_email="tanmaybhaise143@gmail.com",
     packages=find_packages(),
-    install_rquires=get_reuirements()
+    install_rquires=get_reuirements(),
 )
